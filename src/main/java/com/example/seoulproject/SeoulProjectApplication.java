0458362732b20fbff4592abd28ad1b1b9c33aa9a
aaahyunseo <hs030608@naver.com>
2025-05-08 01:10:46 +0900

    feat:SeoulProjectApplication JPA 관련 어노테이션 추가 - #7

--- src/main/java/com/example/seoulproject/SeoulProjectApplication.java
+++ src/main/java/com/example/seoulproject/SeoulProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.seoulproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SeoulProjectApplication {
 
 	public static void main(String[] args) {
