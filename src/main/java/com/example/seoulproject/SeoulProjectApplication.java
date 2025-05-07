3f8c9dd58674e31013d1c66149042bd39f635bc8
aaahyunseo <hs030608@naver.com>
2025-05-07 23:14:26 +0900

    init: 초기 설정

---
+++ src/main/java/com/example/seoulproject/SeoulProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.seoulproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulProjectApplication.class, args);
+	}
+
+}
