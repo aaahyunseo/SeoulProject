d2567a4ab0ada7987659b6d34c58d087601736bb
aaahyunseo <hs030608@naver.com>
2025-05-08 01:10:17 +0900

    feat:UserRepository 이름, 이메일 존재확인 쿼리 추가 - #7

--- src/main/java/com/example/seoulproject/repository/UserRepository.java
+++ src/main/java/com/example/seoulproject/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.example.seoulproject.repository;
 import com.example.seoulproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByName(String name);
+    boolean existsByEmail(String email);
 }
