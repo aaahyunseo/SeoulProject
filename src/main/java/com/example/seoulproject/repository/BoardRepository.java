d2e2611cdda62ed2c40347a10386e377339f9ac3
aaahyunseo <hs030608@naver.com>
2025-05-10 02:43:19 +0900

    feat:BoardRepository id와 user로 게시글 찾기 기능 추가 - #18

--- src/main/java/com/example/seoulproject/repository/BoardRepository.java
+++ src/main/java/com/example/seoulproject/repository/BoardRepository.java
@@ -1,9 +1,12 @@
 package com.example.seoulproject.repository;
 
 import com.example.seoulproject.entity.Board;
+import com.example.seoulproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BoardRepository extends JpaRepository<Board, UUID> {
+    Optional<Board> findByIdAndUser(UUID boardId, User user);
 }
