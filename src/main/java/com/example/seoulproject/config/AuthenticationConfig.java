276bae7edbe8e06e3122653085e420fb17db3de1
aaahyunseo <hs030608@naver.com>
2025-05-10 02:41:30 +0900

    feat:AuthenticationConfig 수정 - #18

--- src/main/java/com/example/seoulproject/config/AuthenticationConfig.java
+++ src/main/java/com/example/seoulproject/config/AuthenticationConfig.java
@@ -20,7 +20,7 @@ public class AuthenticationConfig implements WebMvcConfigurer {
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
                 .addPathPatterns("/users/**")
-                .excludePathPatterns();
+                .excludePathPatterns("/boards");
     }
 
     @Override
