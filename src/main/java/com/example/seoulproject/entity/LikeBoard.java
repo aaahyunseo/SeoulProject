9a9c07c889c74e978596cbb179c6bebe011eb569
aaahyunseo <hs030608@naver.com>
2025-05-09 19:59:22 +0900

    feat:LikeBoard 엔티티 추가 - #15

---
+++ src/main/java/com/example/seoulproject/entity/LikeBoard.java
@@ -0,0 +1,23 @@
+package com.example.seoulproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "like-boards")
+public class LikeBoard extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+}
