5da909cd929e192897f67fdd04a40bef1261e380
aaahyunseo <hs030608@naver.com>
2025-05-10 03:05:46 +0900

    feat:LikeBoardRepository 수정 - #18

--- src/main/java/com/example/seoulproject/repository/LikeBoardRepository.java
+++ src/main/java/com/example/seoulproject/repository/LikeBoardRepository.java
@@ -1,9 +1,14 @@
 package com.example.seoulproject.repository;
 
+import com.example.seoulproject.entity.Board;
 import com.example.seoulproject.entity.LikeBoard;
+import com.example.seoulproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LikeBoardRepository extends JpaRepository<LikeBoard, UUID> {
+    boolean existsByUserAndBoard(User user, Board board);
+    Optional<LikeBoard> findByUserAndBoard(User user, Board board);
 }
