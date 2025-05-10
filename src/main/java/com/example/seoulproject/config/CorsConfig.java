b944b658e1f6004674a89134e82d6674126fc3f2
aaahyunseo <hs030608@naver.com>
2025-05-11 04:27:21 +0900

    feat:CorsConfig 오류 수정 - #22

--- src/main/java/com/example/seoulproject/config/CorsConfig.java
+++ src/main/java/com/example/seoulproject/config/CorsConfig.java
@@ -1,8 +1,12 @@
 package com.example.seoulproject.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,19 +14,35 @@ import java.util.List;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    private final long MAX_AGE_SECS = 3600;
+    private final long MAX_AGE_SECS = 3600L;
     @Value("${client.host}")
     private List<String> clientHosts;
+    private final List<String> allowedMethods = List.of(
+            "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE"
+    );
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        CorsConfiguration config = new CorsConfiguration();
+        clientHosts.forEach(config::addAllowedOrigin);
+        allowedMethods.forEach(config::addAllowedMethod);
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.setMaxAge(MAX_AGE_SECS);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+
+        return filterBean;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(clientHosts.toArray(new String[0]))
-                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-                        HttpMethod.PATCH.name(), HttpMethod.DELETE.name(),
-                        HttpMethod.OPTIONS.name())
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
+                .allowedOrigins("http://localhost:3000") // 클라이언트 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
