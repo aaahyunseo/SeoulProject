efe38a44c1443bf1df031e0a27fd1ad8d310cc5c
aaahyunseo <hs030608@naver.com>
2025-05-12 00:02:28 +0900

    feat:AuthenticationConfig 댓글 api 인터셉터 추가 - #32

--- src/main/java/com/example/seoulproject/config/AuthenticationConfig.java
+++ src/main/java/com/example/seoulproject/config/AuthenticationConfig.java
@@ -20,7 +20,7 @@ public class AuthenticationConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
-                .addPathPatterns("/users/**", "/boards/**")
+                .addPathPatterns("/users/**", "/boards/**", "/comments/**")
                 .excludePathPatterns("/api/**", "/boards/all", "/boards/top", "/boards/one/{boardId}");
     }
 
